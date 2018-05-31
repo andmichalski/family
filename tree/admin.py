c2c6ba0124783a5fa5f3aba7514ce5c7e8910564
andmichalski <andrzmichal@gmail.com>
2018-05-31 11:09:35 +0200

    Add custom last name action

--- tree/admin.py
+++ tree/admin.py
@@ -5,6 +5,8 @@ from django.contrib import admin
 from datetime import datetime
 from django.shortcuts import render
 from django.core import serializers
+from django.http import HttpResponseRedirect
+from django import forms
 
 # Register your models here.
 
@@ -17,7 +19,7 @@ class ChildAdmin(admin.ModelAdmin):
     list_display = ['name', 'last_name', 'birth']
     date_hierarchy = 'birth'
     raw_id_fields = ("father",)
-    actions = ["change_last_name_michalski", "export_to_json"]
+    actions = ["change_last_name_michalski", "export_to_json", "change_lastname"]
 
     def change_last_name_michalski(self, request, queryset):
         queryset.update(last_name="Michalski")
@@ -30,6 +32,23 @@ class ChildAdmin(admin.ModelAdmin):
             f.write(data)
 
     export_to_json.short_description = "Export queryset to json"
+
+    class LastNameForm(forms.Form):
+        _last_name = forms.CharField(max_length=50)
+
+    def change_lastname(self, request,queryset):
+        form = None
+        if 'apply' in request.POST:
+            form = UpdateLastNameForm(request.POST)
+            new_last_name = request.POST['_last_name']
+            queryset.update(last_name=new_last_name)
+            self.message_user(request, "Changed in selected records last name to {}".format(new_last_name))
+            return HttpResponseRedirect(request.get_full_path())
+        if not form:
+            form = self.LastNameForm()
+        return render(request, "admin/tree/child/change_last_name.html", {'users': queryset, 'form':form})
+
+    change_lastname.short_description = "Change to custom last name"
 
 @admin.register(Father)
 class FatherAdmin(admin.ModelAdmin):
